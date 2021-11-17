3c790a47310d4514c8db4aeb261b63a5496d01e5
MolAICal <molaical@yeah.net>
2021-11-17 22:31:24 +0800

    update by bai

--- 015-bindingaffinityPafnucy/list/run.sh
+++ 015-bindingaffinityPafnucy/list/run.sh
@@ -4,7 +4,7 @@ i=0
 cat list.txt | while read line
 do
 	echo $line
-	~/bai/soft/moaicalv11/molaical.exe -model pafnucy -l $line -p pocket.mol2 -o $line'.csv'
+	/home/feng/tutorial/MolAICal-linux64/molaical.exe -model pafnucy -l $line -p pocket.mol2 -o $line'.csv'
 	let i+=1
 done
 
